d72b2af7f329836b20ba103088cc26f17e75ca00
aki <aki@my_mail.com>
2022-02-11 15:09:12 +0900

    getopt()理解した!

--- ss7/getopt.c
+++ ss7/getopt.c
@@ -6,27 +6,34 @@ int main(int argc, char* argv[]){
 
     int opt;
 
-    while((opt = getopt(argc,argv,"af:t")) != -1){
+    int num = 666;
+
+    while((opt = getopt(argc,argv,"afg:")) != -1){
 
 	switch(opt){
 
 	    case 'a':
 		// case a
+	        num = 114;	
 		break;
 
 	    case 'f':
 		// case b
-		break;
-		
-	    case 't':
-		// case t
+	        num =514;	
 		break;
 
+	    case 'g':
+		// case g
+                num = atoi(optarg);		
+		break;
+		
 	    case '?':
 		// error
 		break;
 	}
     }
+
+    printf("num = %d\n",num);
 
     // 何か処理
 }
