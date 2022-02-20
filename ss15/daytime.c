3747cde5205c8014a7b0ee24cc8be1499dfd1e92
aki <aki@my_mail.com>
2022-02-21 01:17:28 +0900

    main関数はok

---
+++ ss15/daytime.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+
+static int open_connction(char* host,char* service);
+
+int main(int argc, char* argv[]){
+
+    int sock;
+    FILE* f;
+    char buf[1024];
+
+    sock = open_connection((argc > 1 ? argv[1]:"localhost"),"daytime");
+    f = fdopen(sock,"r");
+    if(!f){
+
+	perror("fdopen(3)");
+	exit(1);
+    }
+
+    fgets(buf,sizeof(buf),f);
+    fclose(f);
+    fputs(buf,stdout);
+    exit(0);
+}
