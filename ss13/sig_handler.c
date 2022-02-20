a5e5733b9fcdd856c9ede88c4e634feed88e939e
aki <aki@my_mail.com>
2022-02-20 22:35:20 +0900

    説明を追加

--- ss13/sig_handler.c
+++ ss13/sig_handler.c
@@ -6,8 +6,11 @@ sighandler_t trap_signal(int sig, singhandler_t handler){
 
     struct sigaction act, old;
 
+    //(1)シグナルハンドラーをsa_handlerに設定する。
     act.sa_handler = handler;
+    //(2) sigemmptyset()でsa_maskを空に初期化
     sigemptyset(&act.sa_mask);
+    //(3)　自動的な再起動を有効化するためにsa_flagsにSA_RESTARTを設定する。
     sa.sa_flags = SA_RESTART;
     if(sigaction(sig,&act,&old) < 0)
 	return NULL;
