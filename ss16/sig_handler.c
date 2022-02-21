94bb87933c9eeb8de5ab7a0a69ebee0e65f0cdb9
aki <aki@my_mail.com>
2022-02-21 22:48:05 +0900

    sighandler ok

--- ss16/sig_handler.c
+++ ss16/sig_handler.c
@@ -9,4 +9,12 @@ static void trap_signal(int sig, sighandler_t handler){
 
     act.sa_handler = handler;
     sigempty(&act.sa_mask);
+    act.sa_flags = SA_RESTART;
+    if(sigaction(sig,&act,NULL) < 0)
+	log_exit("sigaction()failed: %s",strerror(errno));
+}
+
+static void signal_exit(int sig){
+
+    log_exit("exit by signal %d",sig);
 }
