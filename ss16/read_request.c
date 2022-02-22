e3f53701736d476978faf55f43433818a5160220
aki <aki@my_mail.com>
2022-02-22 21:28:37 +0900

    read request ok

--- ss16/read_request.c
+++ ss16/read_request.c
@@ -14,6 +14,15 @@ static struct HTTPRequest* read_request(FILE* in){
     req->length = content_length(req);
     if(req->length != 0){
 
-	if(req->length)
+	if(req->length > MAX_REQUEST_BODY_LENGTH)
+	    log_exit("request body too long");
+	req->body = xmalloc(req->length);
+	if(fread(req->body,req->length,1,in) < 1)
+	    log_exit("failed to read request body");
+    }else{
+
+	req->body = NULL;
     }
+
+    return req;
 }
